ee34ee65bb74939d088826af5a827e7b04c1966a
Noushhhh <ana.andric@live.fr>
2023-04-19 15:46:43 +0200

    commented

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,21 +1,22 @@
+# Change directory to the WordPress root directory
 cd /var/www/html/wordpress
 
-# DOWNLOAD WORDPRESS FILES
-wp core download  --path=/var/www/html/wordpress --allow-root
+# Download WordPress core files to the specified directory
+wp core download --path=/var/www/html/wordpress --allow-root
 
-# GENERATE AND CONFIGURE THE WP-CONFIG.PHP FILE
+# Generate the wp-config.php file and configure it with the specified database name, username, password, host, and table prefix
 wp config create --path=/var/www/html/wordpress --allow-root --dbname=$DB_DATABASE --dbuser=$DB_USER --dbpass=$DB_USER_PASSWORD --dbhost=$DB_HOST --dbprefix=wp_
 
-# RUNS THE STANDARD WORDPRESS INSTALLATION PROCESS - CREATE THE WORDPRESS TABLES IN DB 
+# Install WordPress core files with the specified site URL, site title, administrator username, password, and email address
 wp core install --path=/var/www/html/wordpress --allow-root --url=$DOMAIN_NAME --title="$WP_SITE_TITLE" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
 
-# CREATE USER IN WORDPRESS
+# Create a user with the specified username, email address, and password
 wp plugin update --allow-root --all
 wp user create --path=/var/www/html/wordpress --allow-root $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD
 
-# GIVE PERMISSIONS TO GROUP AND USER WWW-DATA TO UPLOADS | -R EXECUTE ROOT
+# Set permissions for the uploads directory to the web server user (www-data)
 chown www-data:www-data /var/www/html/wordpress/wp-content/uploads -R
 
-# LAUNCH PHP-FPM
+# Launch PHP-FPM by creating a directory for the PHP-FPM socket and running the PHP-FPM process in foreground mode
 mkdir -p /run/php/
-php-fpm7.3 -F 
+php-fpm7.3 -F
